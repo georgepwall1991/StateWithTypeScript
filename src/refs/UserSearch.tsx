416c8577e31224d599e410a005b3fc4ed23226f5
George Wall <georgewall1991@icloud.com>
2022-08-29 16:59:47 +0100

    Section 3 - Episode 23 - finish up refs

--- src/refs/UserSearch.tsx
+++ src/refs/UserSearch.tsx
@@ -1,35 +1,42 @@
-import React, {useState, useRef } from "react";
+import React, {useState, useRef, useEffect} from "react";
 
 const users = [{name: 'Sarah', age: 20}, {name: 'Alex', age: 20}, {name: 'Michael', age: 20},
 
 ];
 
 const UserSearch: React.FC = () => {
-    const inputRef = useRef<HTMLInputElement | null>(null);
+    const inputRef = useRef<any>(null);
     const [personName, setPersonName] = useState<string>('');
     const [user, setUser] = useState<{ name: string, age: number } | undefined>()
 
-const onClick = () => {
-    const foundUser = users.find((user) => {
-        return user.name === personName;
-    });
+    useEffect(() => {
+        if (!inputRef.current) {
+            return;
+        }
+        inputRef.current.focus()
+    }, []);
 
-    setUser(foundUser);
-};
+    const onClick = () => {
+        const foundUser = users.find((user) => {
+            return user.name === personName;
+        });
+
+        setUser(foundUser);
+    };
 
 
-return (<div>
-    User Search
-    <input ref={inputRef}
-           value={personName}
-           onChange={e => setPersonName(e.target.value)}
-    />
-    <button onClick={onClick}>Find User</button>
-    <div>
-        {user && user.name}
-        {user && user.age}
-    </div>
-</div>);
+    return (<div>
+        User Search
+        <input ref={inputRef}
+               value={personName}
+               onChange={e => setPersonName(e.target.value)}
+        />
+        <button onClick={onClick}>Find User</button>
+        <div>
+            {user && user.name}
+            {user && user.age}
+        </div>
+    </div>);
 
 }
 
