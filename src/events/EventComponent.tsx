ededb332bbd5b04c6272bc43752f46fb804af34e
George Wall <georgewall1991@icloud.com>
2022-08-29 16:44:01 +0100

    Section 3 - Episode 2 - drag events

--- src/events/EventComponent.tsx
+++ src/events/EventComponent.tsx
@@ -5,8 +5,13 @@ const EventComponent: React.FC = () => {
     const onChanged = (event : React.ChangeEvent<HTMLInputElement>) => {
         console.log(event);
     }
+    const onDragStart = (dragEvent : React.DragEvent<HTMLDivElement>) => {
+        console.log(dragEvent);
+    };
+
     return <div>
         <input onChange={onChanged}/>
+        <div draggable onDragStart={onDragStart}>Drag Me!</div>
     </div>
 };
 
